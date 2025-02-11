fa0cba8cca3c214e65f313e7494de620790cba9e
fdhhhdjd <nguyentientai10@gmail.com>
2025-02-11 20:17:02 +0700

    feat: add new code register:

--- src/pages/auth/register/index.jsx
+++ src/pages/auth/register/index.jsx
@@ -1,12 +1,66 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Loading from "../../../components/loading/loading";
+import axiosInstance from "../../../libs/axiosInterceptor";
 import "../../../styles/auth/register.css";
-import { showSuccessToast } from "../../../utils/toastNotifications";
+import { isValidEmail } from "../../../utils/checkInput";
+import {
+  showErrorToast,
+  showSuccessToast,
+} from "../../../utils/toastNotifications";
 
 const Register = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  const navigate = useNavigate();
+
+  // Function to show/hide password
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  // Function to show/hide confirm password
+  const handleShowConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
+  // Function to handle sign up
   const handleSignUp = (e) => {
     e.preventDefault();
-    showSuccessToast("dang nhap thanh cong");
+    setLoading(true);
+
+    if (!isValidEmail(email)) {
+      setLoading(false);
+      return showErrorToast("Invalid email!");
+    }
+
+    // Check if passwords match
+    if (password !== confirmPassword) {
+      setLoading(false);
+      return showErrorToast("Passwords do not match!");
+    }
+
+    axiosInstance
+      .post("/auth/register", {
+        email,
+        password,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setLoading(false);
+          navigate("/auth/login");
+          return showSuccessToast("Account created successfully!");
+        }
+      })
+      .catch((error) => {
+        setLoading(false);
+        return showErrorToast(error.response.data.message);
+      });
   };
 
   return (
@@ -18,36 +72,55 @@ const Register = () => {
             <div className="form-group">
               <i className="fas fa-envelope icon" />
               <input
-                type="email"
+                type="text"
                 id="email"
                 name="email"
                 placeholder="Enter your email"
-                required=""
+                onChange={(e) => setEmail(e.target.value)}
+                required
               />
             </div>
             <div className="form-group">
-              <i className="fas fa-lock icon" />
+              <div onClick={handleShowPassword}>
+                {showPassword ? (
+                  <i className="fas fa-lock icon" />
+                ) : (
+                  <i className="fa-solid fa-unlock icon"></i>
+                )}
+              </div>
               <input
-                type="password"
+                type={showPassword ? "text" : "password"}
                 id="password"
                 name="password"
                 placeholder="Create a password"
-                required=""
+                onChange={(e) => setPassword(e.target.value)}
+                required
               />
             </div>
             <div className="form-group">
-              <i className="fas fa-lock icon" />
+              <div onClick={handleShowConfirmPassword}>
+                {showConfirmPassword ? (
+                  <i className="fas fa-lock icon" />
+                ) : (
+                  <i className="fa-solid fa-unlock icon"></i>
+                )}
+              </div>
               <input
-                type="password"
+                type={showConfirmPassword ? "text" : "password"}
                 id="confirm-password"
                 name="confirm-password"
                 placeholder="Confirm your password"
-                required=""
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
               />
             </div>
-            <button type="submit" className="register-btn">
-              Sign Up
-            </button>
+            {loading ? (
+              <Loading />
+            ) : (
+              <button type="submit" className="register-btn">
+                Sign Up
+              </button>
+            )}
           </form>
           <p className="alternate-option">
             Already have an account? <Link to="/auth/login">Login here</Link>
