6926da6aa7e186516e5bdec28074d490f3ccf451
fdhhhdjd <nguyentientai10@gmail.com>
2025-02-13 12:25:02 +0700

    feat: new code class today

--- src/pages/auth/login/index.jsx
+++ src/pages/auth/login/index.jsx
@@ -1,8 +1,48 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Loading from "../../../components/loading/loading";
+import axiosInstance from "../../../libs/axiosInterceptor";
 import "../../../styles/auth/login.css";
+import {
+  showErrorToast,
+  showSuccessToast,
+} from "../../../utils/toastNotifications";
 
 const Login = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const navigate = useNavigate();
+
+  // Function to show/hide password
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  // Function to handle sign up
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    axiosInstance
+      .post("/auth/login", {
+        identify: email,
+        password,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setLoading(false);
+          navigate("/");
+          return showSuccessToast("Login successfully!");
+        }
+      })
+      .catch((error) => {
+        setLoading(false);
+        return showErrorToast(error.response.data.message);
+      });
+  };
+
   return (
     <React.Fragment>
       <div className="auth">
@@ -10,22 +50,30 @@ const Login = () => {
           <div className="login-box">
             <h2>Welcome Back</h2>
             <p>Login to your account</p>
-            <form action="#">
+            <form onSubmit={handleSignIn}>
               <div className="input-group">
                 <i className="fa fa-envelope icon" />
                 <input
                   type="email"
                   id="email"
                   placeholder="Enter your email"
+                  onChange={(e) => setEmail(e.target.value)}
                   required=""
                 />
               </div>
               <div className="input-group">
-                <i className="fa fa-lock icon" />
+                <div onClick={handleShowPassword}>
+                  {showPassword ? (
+                    <i className="fas fa-lock icon" />
+                  ) : (
+                    <i className="fa-solid fa-unlock icon"></i>
+                  )}
+                </div>
                 <input
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   id="password"
                   placeholder="Enter your password"
+                  onChange={(e) => setPassword(e.target.value)}
                   required=""
                 />
               </div>
@@ -37,9 +85,13 @@ const Login = () => {
                   Forgot Password?
                 </Link>
               </div>
-              <button type="submit" className="login-btn">
-                Login
-              </button>
+              {loading ? (
+                <Loading />
+              ) : (
+                <button type="submit" className="login-btn">
+                  Login
+                </button>
+              )}
             </form>
             <div className="social-login">
               <p>Or login with</p>
